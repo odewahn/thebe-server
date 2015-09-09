4f9cfee36ba7a602d23598e768c4261739fe21c7
Andrew Odewahn <odewahn@oreilly.com>
2015-09-09 14:32:27 -0400

    super basic

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"log"
-	"os"
-	"github.com/joho/godotenv"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func main() {
@@ -13,20 +13,19 @@ func main() {
 	// Load the environment variables we need
 	err := godotenv.Load()
 	if err != nil {
-    	log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file")
 	}
-
 
 	// Read the port
 	port := os.Getenv("PORT")
 
 	mux := mux.NewRouter()
-  // mux.HandleFunc("/events", get_events(dbmap)).Methods("GET")
-  // mux.HandleFunc("/events/{year}", get_events_by_year(dbmap)).Methods("GET")
-
-  n := negroni.Classic()
-  n.UseHandler(mux)
+	// mux.HandleFunc("/events", get_events(dbmap)).Methods("GET")
+	// mux.HandleFunc("/events/{year}", get_events_by_year(dbmap)).Methods("GET")
+	mux.HandleFunc("/spawn", spawn()).Methods("GET")
+	n := negroni.Classic()
+	n.UseHandler(mux)
 	log.Printf("Listening on port %s\n", port)
-  n.Run(":" + port)
+	n.Run(":" + port)
 
 }
---
+++ spawn.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Spawn struct {
+	Url string `json:"url"`
+}
+
+// From https://www.socketloop.com/tutorials/golang-how-to-generate-random-string
+func randStr() string {
+	dictionary := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, 12)
+	rand.Read(bytes)
+	for k, v := range bytes {
+		bytes[k] = dictionary[v%byte(len(dictionary))]
+	}
+	return string(bytes)
+}
+
+func spawn() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		url := fmt.Sprintf("%s.%s", randStr(), os.Getenv("THEBE_SERVER_BASE_URL") )
+		s := Spawn{ Url: url }
+		dat, err := json.MarshalIndent(&s, "", "  ")
+		fmt.Println(dat)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, string(dat))
+	}
+}
